4fd25a5dce7bd4a2b088647073a1aaade692f6cf
gopalkumar1 <52914965+gopalkumar1@users.noreply.github.com>
2023-04-11 23:00:24 +0530

    Create power.py

---
+++ power.py
@@ -0,0 +1,17 @@
+# import main Flask class and request object
+from flask import Flask, request
+
+# create the Flask app
+app = Flask(__name__)
+
+@app.route('/power')
+def power():
+    num1 = request.args.get('num')
+    # calculate the square of num1
+    result = int(num1) ** 2
+    # return the result as a string
+    return str(result)
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
